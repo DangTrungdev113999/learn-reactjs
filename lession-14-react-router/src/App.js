a607c7d2818e73cbf3e4b018debb807578593f85
dangtrungdev113999 <dangtrungdev113999@gmail.com>
2019-07-17 19:22:15 +0700

    Custom Link

--- lession-14-react-router/src/App.js
+++ lession-14-react-router/src/App.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+
+const MenuLink = ({ lable, to, activeOnlyWhenExact }) => {
+    return (
+        <Route
+            path={to}
+            exact={activeOnlyWhenExact}
+            children={({match}) => {
+                let active = match ? 'active abc' : '';
+                return (
+                    <li className={active}>
+                        <Link to={to}>
+                            {lable}
+                        </Link>
+                    </li>
+                );
+            }}
+        />
+    );
+}
 
 class App extends Component {
 
@@ -13,17 +32,11 @@ class App extends Component {
             <Router>
                 <div className="App">
                     
-                    <nav class="navbar navbar-inverse">
-                        <ul class="nav navbar-nav">
-                            <li >
-                                <NavLink exact activeClassName='active' to="/" className=''>Home</NavLink>
-                            </li>
-                            <li>
-                                <NavLink activeClassName='active' to="/about" className=''>About</NavLink>
-                            </li>
-                            <li>
-                                <NavLink activeClassName='active' to="/contact" className=''>contact</NavLink>
-                            </li>
+                    <nav className="navbar navbar-default">
+                        <ul className="nav navbar-nav">
+                            <MenuLink lable='Home' to='/' activeOnlyWhenExact={true} />
+                            <MenuLink lable='About' to='/about' activeOnlyWhenExact={false} />
+                            <MenuLink lable='Contact' to='/contact' activeOnlyWhenExact={false} />
                         </ul>
                     </nav>
 
